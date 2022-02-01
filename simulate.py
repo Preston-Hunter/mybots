632f6402c0faa1eb7f6a3e002612608fcd5926ae
BuildTools <unconfigured@null.spigotmc.org>
2022-02-01 09:51:32 -0500

    end of joints

--- simulate.py
+++ simulate.py
@@ -1,9 +1,15 @@
 import pybullet as p
 import time
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.loadSDF("box.sdf")
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
+
+p.loadSDF("world.sdf")
 for _ in range(10000):
     p.stepSimulation()
     time.sleep(1.0/60)
