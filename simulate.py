6da9c6a4c935162eca434d74776d1764fe4423dd
BuildTools <unconfigured@null.spigotmc.org>
2022-02-08 10:16:19 -0500

    motors

--- simulate.py
+++ simulate.py
@@ -3,10 +3,11 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy as numpy
+
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.setGravity(0,0,-9.8)
+p.setGravity(0, 0, -9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 
@@ -21,8 +22,8 @@ for _ in range(100):
     backLegSensorValues[_] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[_] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     print(frontLegSensorValues[_])
-    time.sleep(1.0/60)
-    #print(_)
+    time.sleep(1.0 / 60)
+    # print(_)
 numpy.save("data/backLegSensorValues.npy", backLegSensorValues)
 numpy.save("data/frontLegSensorValues.npy", frontLegSensorValues)
 
