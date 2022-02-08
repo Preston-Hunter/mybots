06a22f3e885895630091017796faf65218500862
BuildTools <unconfigured@null.spigotmc.org>
2022-02-08 10:33:45 -0500

    jointInfo utf change

--- simulate.py
+++ simulate.py
@@ -14,16 +14,23 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = numpy.zeros(100)
-frontLegSensorValues = numpy.zeros(100)
+backLegSensorValues = numpy.zeros(10000)
+frontLegSensorValues = numpy.zeros(10000)
 
-for _ in range(100):
+for _ in range(10000):
     p.stepSimulation()
     backLegSensorValues[_] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[_] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robotId,
+        jointName="Torso_BackLeg",
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=0.0,
+        maxForce=500)
     print(frontLegSensorValues[_])
     time.sleep(1.0 / 60)
     # print(_)
+
 numpy.save("data/backLegSensorValues.npy", backLegSensorValues)
 numpy.save("data/frontLegSensorValues.npy", frontLegSensorValues)
 
